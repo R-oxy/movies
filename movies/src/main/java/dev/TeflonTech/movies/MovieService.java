575bcaa8043bc6dbd6a1dc68bb806fd80e515859
R-oxy <118716832+R-oxy@users.noreply.github.com>
2023-04-24 07:48:17 -0500

    Adds the movies  and reviews api endpoints

--- movies/src/main/java/dev/TeflonTech/movies/MovieService.java
+++ movies/src/main/java/dev/TeflonTech/movies/MovieService.java
@@ -1,6 +1,5 @@
 package dev.TeflonTech.movies;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +14,7 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
-    public Optional<Movie> singleMovie(ObjectId id) {
-        return movieRepository.findById(id);
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByimdbId(imdbId);
     }
 }
--- movies/src/main/java/dev/TeflonTech/movies/ReviewRepository.java
+++ movies/src/main/java/dev/TeflonTech/movies/ReviewRepository.java
@@ -1,12 +1,9 @@
 package dev.TeflonTech.movies;
 
-import java.util.Optional;
-
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-    Optional<Movie> findMovieByimdbId(String imdbId);
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
